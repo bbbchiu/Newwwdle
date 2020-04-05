c2e9554c5bec29b396f163f52171b57de3c7b417
jameswang0 <jason980160@gmail.com>
2020-04-05 21:58:57 +0800

    add list

--- app/src/main/java/com/example/newwwdle/MainActivity.java
+++ app/src/main/java/com/example/newwwdle/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.newwwdle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText name, password;
+    private Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.edit_name);
+        password = findViewById(R.id.edit_id);
+
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Student.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", name.getText().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
